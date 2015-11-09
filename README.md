16a0fdbebbcdbee540d9891017b015987c4f5089
NanaRT <alanataylor@hotmail.fr>
2015-11-09 21:13:50 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Asso-B2G
+========
+
+A Symfony project created on November 9, 2015, 6:14 pm.
---
+++ src/AssoB2GBundle/AssoB2GBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AssoB2GBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AssoB2GBundle extends Bundle
+{
+}
